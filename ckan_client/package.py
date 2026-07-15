b13639b4ca7d4417bfcf42f682068dc378e59166
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2026-07-15 14:29:05 +0200

    feat: package has resources

--- ckan_client/package.py
+++ ckan_client/package.py
@@ -1,5 +1,14 @@
 from .base_object import BaseObject
+from .resource import Resource
 
 
 class Package(BaseObject):
-  pass
+    @property
+    def resources(self):
+        self._fetch_metadata()
+        for res in self._attrs["resources"]:
+            yield Resource(
+                id=res["id"],
+                _client=self._client,
+                _from_response=res,
+            )
