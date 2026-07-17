7c11ca3de49bbbbe3d8dfe378636c3da10362959
Pierlou <pierlou.ramade@protonmail.com>
2026-07-17 15:32:15 +0000

    chore: add orga tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,7 @@ from ckan_client import CkanClient
 
 CKAN_URL = "https://ckan.data.example.fr"
 ORGANIZATION_ID = "e9d6a73f-dd4d-4c89-abe4-be24d31b9239"
-ORGANIZATION_NAME = "my-orga"
 PACKAGE_ID = "e585b3d4-3c5b-4b95-948e-6f6ec27e922a"
-PACKAGE_NAME = "new_dataset"
 RESOURCE_ID = "e7a249c8-cfc9-4608-b5d2-ef0541255ba9"
 
 with open("tests/package_metadata.json", "r") as f:
