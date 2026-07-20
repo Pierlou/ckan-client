dccc8cfc923f95a2dea8a2ad3660c8b918b2a0fa
Pierlou <pierlou.ramade@protonmail.com>
2026-07-20 10:47:24 +0200

    chore: add creation from client tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,10 @@
+from unittest.mock import Mock
+
 import pytest
 from conftest import CKAN_URL
 
-from ckan_client.client import CkanClient, check_kwargs
+from ckan_client import CkanClient, Organization, Package, Resource
+from ckan_client.client import check_kwargs
 
 
 @pytest.mark.parametrize(
@@ -72,3 +75,27 @@ def test_client(mock_help, apikey):
             isinstance(v, dict) and all(k in v for k in ["type", "description", "optional"])
             for v in ckanc._obj_params[obj].values()
         )
+
+
+@pytest.mark.parametrize(
+    "obj_class, payload",
+    [
+        (Organization, {"name": "My orga", "description": "Short description"}),
+        (Package, {"name": "A new dataset", "private": True, "tags": ["test"]}),
+        (Resource, {"name": "New resource", "upload": "an open file", "mimetype": "text/csv"}),
+    ],
+)
+def test_client_create_obj(mock_help, obj_class, payload):
+    ckanc = CkanClient(CKAN_URL, apikey="s3cr3t")
+    _id = "abcd"
+    _name = obj_class.__name__.lower()
+    setattr(
+        ckanc.rckan.action,
+        f"{_name}_create",
+        Mock(return_value=payload | {"id": _id}),
+    )
+    created = getattr(ckanc, f"create_{_name}")(payload)
+    assert isinstance(created, obj_class)
+    assert created.id == _id
+    for k, v in payload.items():
+        assert getattr(created, k) == v
